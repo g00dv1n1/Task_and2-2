7c4567428af563f40e90e633a6874495c49bd47f
Vladislav Schiller <vladislav.shiller@gmail.com>
2020-11-19 17:56:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task2_2/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.task2_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView resultText = findViewById(R.id.resultString);
+        int a = 1;
+        int b = 100;
+        int random_number1 = a + (int) (Math.random() * b);
+        String text = " http://myfile.org/" + random_number1;
+        resultText.setText(text);
+
+        Button buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
